df22f51f778a64ae42044c5544e3a12bc03edd9f
Mohy Elden <Mohieelden50@gmail.com>
2024-10-06 21:59:48 +0300

    Set up Prisma to be exported for use across controllers.

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -2,6 +2,4 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient()
 
-const users = prisma.user.findMany({});
-console.log(users)
-module.exports = users
+module.exports = prisma

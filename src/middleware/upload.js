20e25da5d5baae181dc58b1c138b8ac423358239
Mohy Elden <Mohieelden50@gmail.com>
2024-10-13 19:02:53 +0300

    multer upload middleware of course

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const path = require('path');
+const { BadRequestError } = require('../errors');
 
 const storage = multer.diskStorage({
     destination: './uploads/',
@@ -8,8 +9,22 @@ const storage = multer.diskStorage({
     }
 });
 
+const fileFilter = (req, file, cb) => {
+    try {
+        if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new BadRequestError('Unsupported file type. Please upload a PNG or JPEG image.'), false);
+        }
+    } catch (err) {
+        cb(err, false);
+    }
+};
+
+
 const upload = multer({
     storage: storage,
+    fileFilter: fileFilter,
     limits: { fileSize: 1000000 } // 1MB file size limit
 }).single('file'); // 'myFile' is the name attribute of the file input field
 
